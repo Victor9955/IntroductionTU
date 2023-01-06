880b8e6e1568924355a40cde795c580325f4260e
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2023-01-06 17:30:35 +0100

    added running aniamtion

--- Assets/AnimatorBinding.cs
+++ Assets/AnimatorBinding.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimatorBinding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] InputActionReference _move;
+    Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        _move.action.performed += MoveAnim;
+        _move.action.canceled += StopMoveAnim;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StopMoveAnim(InputAction.CallbackContext obj)
     {
-        
+        animator.SetBool("Walking", false);
+    }
+
+    private void MoveAnim(InputAction.CallbackContext obj)
+    {
+        Vector2 JoystickDirection = obj.ReadValue<Vector2>();
+        animator.SetBool("Walking", true);
+        transform.LookAt(new Vector3(transform.position.x + JoystickDirection.x, transform.position.y, transform.position.z + JoystickDirection.y));
+    }
+
+    private void OnDestroy()
+    {
+        _move.action.performed -= MoveAnim;
+        _move.action.canceled -= StopMoveAnim;
     }
 }

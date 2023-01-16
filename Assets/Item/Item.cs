1316ac354c502dcef75e9c8e58fa5b670e95b837
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2023-01-16 16:32:09 +0100

    Small problem that break other items please take this version

--- Assets/Item/Item.cs
+++ Assets/Item/Item.cs
@@ -6,7 +6,7 @@ public class Item : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player") && Active(other))
+        if(other.gameObject.transform.parent.CompareTag("Player") && Active(other))
         {
             Destroy(gameObject);
         }
